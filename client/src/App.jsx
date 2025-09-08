ab6b436e57b5b9ecdb658a0dfcec1fbe3a1ef761
Gagan jain004 <gagan.praveen004@gmail.com>
2025-09-09 00:01:39 +0530

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import Profile from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain"> 
+      <Routes>
+
+      <Route  path='/' element={<HomePage/>}/>
+      <Route  path='/login' element={<LoginPage/>}/>
+      <Route  path='/profile' element={<Profile/>}/>
+
+      </Routes>
     </div>
   )
 }
